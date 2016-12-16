e39ac95961da4e2b8e63377bc2166b7b8d3b3fee
haswelliris <haswelliris@live.com>
2016-12-16 20:32:20 +0800

    update for saved files

---
+++ H209/js/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('#menu a', { fontFamily: 'FZLanTingHeiS-EL-GB', hover:true });
+Cufon.replace('h2 , h3 , h4' , { fontFamily: 'FZXiDengXian-Z06', hover:true });
+// Cufon.replace('p' , {fontFamily: 'FZLanTingHeiS-EL-GB' , hover:true});
+
